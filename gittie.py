430c6b8849026f5df13cc1e15bfa667db43ac966
Daria Rzepka <dariarz0110@gmail.com>
2018-03-21 11:22:06 +0100

    gittie.py

--- gittie.py
+++ gittie.py
@@ -1,4 +1,11 @@
 def shout():
     print ("aaaaaaaaa!")
-def introduce()
+def introduce():
     print("Hello, I'm Gittie!")
+def add(a,b): 
+    c=a+b
+    return c
+
+shout()
+introduce()
+add(3,5)
